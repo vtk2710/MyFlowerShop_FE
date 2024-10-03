4d4d56783e4f1cc4dedcb35a81ca1fc4355aa829
duckyy1103 <ducvmse182942@fpt.edu.vn>
2024-10-03 08:39:07 +0700

    update css of CommentSlide

--- flower/shop/src/config/firebase.js
+++ flower/shop/src/config/firebase.js
@@ -8,15 +8,6 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyB_mxPyoaJ8KvTRVAdWeB4cwPfVV9ZFlfQ",
-  authDomain: "movies-management-5eaab.firebaseapp.com",
-  projectId: "movies-management-5eaab",
-  storageBucket: "movies-management-5eaab.appspot.com",
-  messagingSenderId: "593035503165",
-  appId: "1:593035503165:web:98c83c8870a6707a9bac54",
-  measurementId: "G-828QK252BT"
-};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
